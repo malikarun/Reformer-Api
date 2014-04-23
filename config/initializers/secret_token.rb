6dc8b0395c21bd90f32541096939c5f58212ffda
Arun Malik <akmalik@ymail.com>
2014-04-24 07:19:00 +1000

    SocialReformer-Api
    api for SocialReformer-App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+SocialReformer::Application.config.secret_token = '9316d8245789b007a7806587d6e17699182c92aa979257f9f79962495548095c7db2a5527bed927349497d52b9aed9aeefd2357d254b5154d678bc7f886c3cc5'
